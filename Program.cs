71fef1c786b1f572cfd9df2e6c47539170c3a83c
saturnnn7 <soprikin.kirill@gmail.com>
2026-04-05 23:22:30 +0200

    init: project folder structure and base Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
